21876a14b0a3f4104bd31d7e264ca8bc4da6457f
Akash Sharma <aakashsharma19090@gmail.com>
2021-08-23 07:25:31 +0530

    Basic JS operations snippets

--- test.js
+++ test.js
@@ -32,20 +32,129 @@ const TuringCalPoints = (arr) =>{
     return totalPoints
 }
 
-console.log(TuringCalPoints(ops))
+// console.log(TuringCalPoints(ops)) 
 
 //  ************* TURING BASEBALL GAME PROBLEM ENDS *************
 
 
 
 
-
 // ? Basic Array Operations
 
-// * 1) Find index of specific item in array
-// * 2) Find Element in array
-// * 3) Find and Replace an item in Array at specific index
-// * 4) Remove item at specific index in array
-// * 5) Swap item at specific index in array
+// * 1) Find index of specific item in array ( 1st Occurence in array )
+// * 2) Find index of specific item in array ( Last Occurence in array )
+// * 2) Find index of specific item in array at all occurences ( At multiple indexes in array  )
 
 
+// // * 2) Find Element in array
+// // * 3) Find and Replace an item in Array at specific index
+// // * 4) Remove item at specific index in array
+// // * 5) Swap item at specific index in array
+
+// * 1) Find index of specific item in array ( 1st Occurence in array )
+
+function indexFirstOccurence(){
+    var items = [
+        {value: 100},
+        {value: 200},
+        {value: 300},   // 1st Occurence
+        {value: 400},
+        {value: 300}   // 2nd Occurence
+    ];
+
+    var index = items.findIndex(item => item.value === 300)
+    console.log('First Occurence Index of 300 -> ', index)
+    
+    return index
+
+}
+// indexFirstOccurence()
+
+
+// * 2) Find index of specific item in array ( Last Occurence in array )
+
+function indexLastOccurence(){
+    var items = [
+        100,
+        200,
+        300,
+        400,
+        300
+    ];
+
+    var index = items.lastIndexOf(300)
+    console.log('Last Occurence Index of 300 -> ', index)
+
+    return index
+
+}
+// indexLastOccurence()
+
+
+// * 3) Find index of specific item in array at all occurences ( At multiple indexes in array  )
+
+function indexMultipleOccurences(){
+    const items = [
+        {value: 100},
+        {value: 200},
+        {value: 300},   // 1st Occurence
+        {value: 400},
+        {value: 300},   // 2nd Occurence
+        {value: 700},
+        {value: 100},
+        {value: 300},
+        {value: 900},
+        {value: 300}
+    ];
+
+    let foundAtIndexes = []
+    
+    items.map( (item, index) => {
+        if(item.value === 300){
+            foundAtIndexes.push(index)
+        }
+    } )
+    console.log('300 Found at these indexes -> ', foundAtIndexes) 
+    return foundAtIndexes
+}
+
+// indexMultipleOccurences() 
+
+
+// * 4) Find Element in array that meets a condition
+
+function elementInArray(){
+    const items = [
+        {value: 100},
+        {value: 200},
+        {value: 300},   // 1st Occurence
+        {value: 400},
+        {value: 300}   // 2nd Occurence
+    ];
+    
+    const foundElement = items.find( item => item.value === 300 )
+    console.log('Item with value 300 is -> ', foundElement);
+
+    return foundElement
+}
+// elementInArray()
+
+
+
+// * 5) Find All Elements in array that meets a condition
+
+function elementsInArray(){
+    const items = [
+        {value: 100},
+        {value: 200},
+        {value: 300},   // 1st Occurence
+        {value: 400},
+        {value: 300}   // 2nd Occurence
+    ];
+    
+    const foundElements = items.filter( item => item.value === 300 )
+    console.log('All Items with value 300 is -> ', foundElements);
+
+    return foundElements
+}
+elementsInArray()
