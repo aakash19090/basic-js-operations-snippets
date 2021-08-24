08b207672fcde878b1cf0b6b595f4d105ba1a160
Akash Sharma <aakashsharma19090@gmail.com>
2021-08-24 13:40:29 +0530

    array operation snippets added

--- test.js
+++ test.js
@@ -39,10 +39,25 @@ var TuringCalPoints = (arr) =>{
 
 // ? Basic Array Operations
 
-// // * 2) Find Element in array
-// // * 3) Find and Replace an item in Array at specific index
-// // * 4) Remove item at specific index in array
-// // * 5) Swap item at specific index in array
+// * 1) Find index of specific item in array ( 1st Occurence in array )
+// * 2) Find index of specific item in array ( Last Occurence in array )
+// * 3) Find index of specific item in array at all occurences ( At multiple indexes in array  )
+// * 4) Find Item in array that meets a condition
+// * 5) Find all Items in array that meets a condition
+// * 6) Replacing an item in Array at any index
+// * 7) Remove item in Array at first Index
+// * 8) Remove item in Array at Last Index
+// * 9) Remove item in Array at any Index
+// * 10) Check if some items in Array pass specific test
+// * 11) Check if every items in Array pass specific test
+// * 12) Reverse an Array
+// * 13) Sorting Array 
+// * 14) Convert Array-like iteratable objects into Arrays
+// * 15) Convert iteratables into Arrays
+// * 16) Flatten nested Array Depths
+// * 17) Extract some items from Array
+
+
 
 // * 1) Find index of specific item in array ( 1st Occurence in array )
 
@@ -55,7 +70,7 @@ function indexFirstOccurence(){
         {value: 300}   // 2nd Occurence
     ];
 
-    var index = items.findIndex(item => item.value === 300)
+    var index = items.findIndex(item => item.value === 300) // ? Can also use 'indexOf(item)' to find index of direct items in array.
     console.log('First Occurence Index of 300 -> ', index)
     
     return index
@@ -114,7 +129,7 @@ function indexMultipleOccurences(){
 // indexMultipleOccurences() 
 
 
-// * 4) Find Element in array that meets a condition
+// * 4) Find Item in array that meets a condition
 
 function elementInArray(){
     const items = [
@@ -134,7 +149,7 @@ function elementInArray(){
 
 
 
-// * 5) Find All Elements in array that meets a condition
+// * 5) Find all Items in array that meets a condition
 
 function elementsInArray(){
     const items = [
@@ -152,7 +167,7 @@ function elementsInArray(){
 
 
 
-// * 6) Replacing an item in Array at specific index
+// * 6) Replacing an item in Array at any index
 
 function replaceAtIndex(){
     const items = [
@@ -207,7 +222,7 @@ function removeAtLastIndex(){
 // removeAtLastIndex()
 
 
-// * 9) Remove item in Array at specific Index
+// * 9) Remove item in Array at any Index
 
 function removeAtIndex(){
     const items = [
@@ -228,3 +243,123 @@ function removeAtIndex(){
     console.log('Another method using Filter() for New array after removing 300 at index 2 is ->',newItems)
 }
 // removeAtIndex()
+
+
+// * 10) Check if some items in Array pass specific test
+
+function checkSomeItems(){
+    const items = [
+        {value: 100},
+        {value: 200},
+        {value: 300},   
+        {value: 400},
+        {value: 500}  
+    ];
+
+    const checkStatus = items.some(item => item.value > 300)
+    console.log('Check if there are atleast some items in array with value > 300 ->', checkStatus)
+    return checkStatus
+}
+// checkSomeItems()
+
+
+
+// * 11) Check if every items in Array pass specific test
+
+function checkSomeItems(){
+    const items = [
+        {value: 100},
+        {value: 200},
+        {value: 300},
+        {value: 400},
+        {value: 500}   
+    ];
+
+    const checkStatus = items.every(item => item.value > 300)
+    console.log('Check if all items in array have value > 300 ->', checkStatus)
+    return checkStatus
+}
+// checkSomeItems()
+
+
+// * 12) Reverse an Array
+
+function reverseArray(){
+    const items = [
+        {value: 100},
+        {value: 200},
+        {value: 300},   
+        {value: 400},
+        {value: 500} 
+    ];
+    console.log( 'Reversed array -> ' ,items.reverse())
+}
+// reverseArray()
+
+
+
+// * 13) Sorting Array 
+
+function sortArray(){
+    const items = [
+        {value: 100},
+        {value: 200},
+        {value: 300},   
+        {value: 400},
+        {value: 500} 
+    ];
+    
+    descOrder = items.sort((a, b) => a.value > b.value ? -1 : 1);
+    ascOrder = items.sort((a, b) => a.value > b.value ? 1 : -1);
+
+    console.log('Sorted Descending ->', descOrder);
+    console.log('Sorted Ascending ->', ascOrder);
+    
+}
+// sortArray()
+
+
+
+// * 14) Convert Array-like iteratable objects into Arrays
+
+function convertArrayLikeToArray() {
+    const name = "Akash"
+    const nameArr = Array.from(name)
+    console.log('Converted array like string into Array ->' ,nameArr);
+}
+// convertArrayLikeToArray()
+
+
+// * 15) Convert iteratables into Arrays
+
+function convertToArray() {
+    const nameArr = Array.of(1,2,3,4,5,6)
+    console.log('Converted interatables into Array ->' ,nameArr);
+}
+// convertToArray()
+
+
+// * 16) Flatten nested Array Depths
+
+function flattenNestedArray(){
+    const items1 = [ 1,2, [3,4], 5,6, [7,8]]
+    const items2 = [ 1,2, [3,4, [5,6] , [7,8,9, [1,2,3,4]]]]
+
+    const flatArr1 = items1.flat()
+    const flatArr2 = items2.flat(3) // ? Flattens 3 level deep
+    
+    console.log(flatArr1)
+    console.log(flatArr2)
+}   
+// flattenNestedArray()
+
+
+
+// * 17) Extract some items from Array
+
+function extractItemsFromArray(){
+    const items = [ 'Batman', 'Superman', 'Spiderman', 'Joker', 'Hulk' ];
+    const splicedArray = items.splice(1,3)
+    console.log(splicedArray)
+}
+// extractItemsFromArray()
